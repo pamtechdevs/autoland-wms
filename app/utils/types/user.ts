6ad05393e2623add003a8b09a8db004bbc633fc5
pamtechdevs <pamtechdev@gmail.com>
2025-03-10 16:40:49 +0100

    Create user.ts

---
+++ app/utils/types/user.ts
@@ -0,0 +1,15 @@
+export interface UsersFormData {
+  name: string;
+  phone: string;
+  workshop: string;
+  email: string;
+  password: string;
+  role: string;
+  status: "On Duty" | "On Leave";
+}
+
+export interface TableProps<T> {
+  data: T[];
+  onEdit: (item: T) => void;
+  onDelete: (item: T) => void;
+}
